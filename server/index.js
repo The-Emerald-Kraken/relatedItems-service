790f62412aca8e08d0496612ac42ab634b87eba6
Logan Smith <lsmith2895@yahoo.com>
2020-09-25 15:28:45 -0700

    general server setup

--- server/index.js
+++ server/index.js
@@ -13,8 +13,23 @@ app.use(express.static(__dirname + '/../public'));
 
 
 app.get('/api/products/:id/relatedItems', (req, res) => {
-    //request 10 random items from the database
+  //request 10 random items from the database
+  //get id from url 
+  //create 9 sequential numbers after the id number
+  // use find on the 10 items and add them to an array
+  //respond with the array of 10
+
+  //experimental
+  //use Items.find({}).limit(10)
+  //res.send(the 10 items as an array)
+
+  Items.find({}).limit(10)
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
   res.send('GET REQUEST RESPONSE')
+  
 })
 
 app.listen(port, () => {
