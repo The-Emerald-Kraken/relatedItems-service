726afec759e84fc9c89eeddca62037e0b7334351
Logan Smith <lsmith2895@yahoo.com>
2020-09-26 15:16:22 -0700

    create a new React component to show related items list and request data from database to be used in population of the list component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,39 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import axios from 'axios';
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productList: [],
+    };
+    this.getProductList = this.getProductList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProductList();
+  }
+
+  getProductList() {
+    axios({
+      method: 'get',
+      url: '/api/products/:id/relatedItems',
+    })
+      .then((data) => { this.setState({ productList: data.data }); console.log(data.data); })
+      .catch((err) => { console.log(err); });
   }
 
   render() {
+    const { productList } = this.state;
     return (
-      <div>HELLO FROM REACT</div>
+      <div>
+        <List list={productList} />
+      </div>
     );
   }
 }
