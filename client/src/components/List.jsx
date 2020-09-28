c4c66422b26da992258f4d8531904b07ad5aa317
Logan Smith <lsmith2895@yahoo.com>
2020-09-28 09:57:48 -0700

    render list of products

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,18 +1,41 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 function List(props) {
-  console.log(props.list[0]);
+  const { list } = props;
+  const resultList = list.map((product) => (
+    <li key={product.product_id}>
+      <div id="related-item-image" src={product.product_url}>
+        {product.product_url}
+      </div>
+      <div id="related-item-manufacturer">
+        {' '}
+        {product.manufacturer}
+      </div>
+      <div id="related-item-product-name">
+        {' '}
+        {product.item_name}
+      </div>
+      <div id="related-item-rating">
+        {' '}
+        {product.rating}
+      </div>
+      <div id="related-item-number-of-ratings">
+        (
+        {product.number_of_ratings}
+        )
+      </div>
+      <div id="related-item-price">
+        $
+        {product.price}
+      </div>
+    </li>
+  ));
+
   return (
     <div id="list-container">
       <ul>
-        <li>
-          <div id="related-item-image"> image</div>
-          <div id="related-item-manufacturer"> manufacturer</div>
-          <div id="related-item-product-name"> name</div>
-          <div id="related-item-rating"> 3.5</div>
-          <div id="related-item-number-of-ratings">`(72)`</div>
-          <div id="related-item-price">$199.99</div>
-        </li>
+        {resultList}
       </ul>
     </div>
   );
