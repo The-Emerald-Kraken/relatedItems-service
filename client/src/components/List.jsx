9d0a305e535ed4e3f903d2841ed14e54dfd0a85c
Logan Smith <lsmith2895@yahoo.com>
2020-09-28 15:25:02 -0700

    change over to styled components

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -3,33 +3,70 @@ import React from 'react';
 
 function List(props) {
   const { list } = props;
-  console.log(list)
-  const resultList = list.map((product) => (
-    <li key={product.product_id}>
-      <img src={product.product_url} alt="a dog" />
-      <div id="related-item-manufacturer">
-        {' '}
-        {product.manufacturer}
-      </div>
-      <div id="related-item-product-name">
-        {' '}
-        {product.item_name}
-      </div>
-      <div id="related-item-rating">
-        {' '}
-        {product.rating}
-      </div>
-      <div id="related-item-number-of-ratings">
-        (
-        {product.number_of_ratings}
-        )
-      </div>
-      <div id="related-item-price">
-        $
-        {product.price}
-      </div>
-    </li>
-  ));
+  console.log(list);
+  const resultList = list.map((product) => {
+    if (product.onSale) {
+      return (
+        <li key={product.product_id}>
+          <img src={product.product_url} alt="a dog" />
+          <div id="related-item-manufacturer">
+            {' '}
+            {product.manufacturer}
+          </div>
+          <div id="related-item-product-name">
+            {' '}
+            {product.item_name}
+          </div>
+          <div id="related-item-rating">
+            {' '}
+            {product.rating}
+          </div>
+          <div id="related-item-number-of-ratings">
+            (
+            {product.number_of_ratings}
+            )
+          </div>
+          <div id="related-item-price">
+            $
+            {product.onSalePrice}
+            {' '}
+            <div id="related-item-price-onsale" style={{ textDecorationLine: 'line-through' }}>
+              $
+              {product.price}
+              .99
+            </div>
+
+          </div>
+        </li>
+      );
+    }
+    return (
+      <li key={product.product_id}>
+        <img src={product.product_url} alt="a dog" />
+        <div id="related-item-manufacturer">
+          {' '}
+          {product.manufacturer}
+        </div>
+        <div id="related-item-product-name">
+          {' '}
+          {product.item_name}
+        </div>
+        <div id="related-item-rating">
+          {' '}
+          {product.rating}
+        </div>
+        <div id="related-item-number-of-ratings">
+          (
+          {product.number_of_ratings}
+          )
+        </div>
+        <div id="related-item-price">
+          $
+          {product.price}
+        </div>
+      </li>
+    );
+  });
 
   return (
     <div id="list-container">
