b01d2eaf7a24304105224271a4293eb5b118390b
Logan Smith <lsmith2895@yahoo.com>
2020-09-28 14:08:13 -0700

    write a few tests to check List and App components

--- __test__/setup.test.js
+++ __test__/setup.test.js
@@ -5,12 +5,57 @@
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
 import App from '../client/src/components/App.jsx';
+import List from '../client/src/components/List.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('A suite', () => {
+describe('App component tests', () => {
   it('should render without throwing an error', () => {
-    expect(shallow(<App />).contains(<div className="foo">Bar</div>)).toBe(false);
+    expect(shallow(<App />).contains(<div id="list-container">Bar</div>)).toBe(false);
+  });
+});
+
+describe('List component tests', () => {
+  it('should render items in the List', () => {
+    const productList = [{
+      product_id: 100,
+      manufacturer: 'Morar Group',
+      item_name: 'Tasty Cotton Chips',
+      rating: 0.753092278577937,
+      number_of_ratings: 496,
+      price: 267,
+      onSalePrice: 225.84,
+      product_url: 'https://fecimagesghrsea12.s3-us-west-1.amazonaws.com/pics/image100.jpg',
+      onSale: true,
+      __v: 0,
+    },
+    {
+      product_id: 90,
+      manufacturer: 'Gusikowski, Kohler and Pacocha',
+      item_name: 'Intelligent Plastic Shoes',
+      rating: 3.655240113863547,
+      number_of_ratings: 345,
+      price: 925,
+      onSalePrice: 785.84,
+      product_url: 'https://fecimagesghrsea12.s3-us-west-1.amazonaws.com/pics/image90.jpg',
+      onSale: true,
+      __v: 0,
+    },
+    {
+      product_id: 89,
+      manufacturer: 'Gleason, Pacocha and Hyatt',
+      item_name: 'Fantastic Metal Towels',
+      rating: 4.510381807800804,
+      number_of_ratings: 0,
+      price: 324,
+      onSalePrice: 274.84,
+      product_url: 'https://fecimagesghrsea12.s3-us-west-1.amazonaws.com/pics/image89.jpg',
+      onSale: true,
+      __v: 0,
+    }];
+    const wrapper = shallow(<List list={productList} />);
+    expect(wrapper.text()).toContain('Morar Group');
   });
 });
